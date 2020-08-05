3fea9e8b125e4957030b55e3abc65218a7a10535
codefanatic2020 <codefanatics2020@gmail.com>
2020-08-05 23:24:10 +0530

    comment changes

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -8,8 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //my first commit
-        
-        //my second commit
+      //
+
     }
 }
